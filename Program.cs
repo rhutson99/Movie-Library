9b13c7e125465e09c5cf9182aa7bdeb481d70073
ryanhutson99 <ryanhutson99@gmail.com>
2021-02-11 08:47:11 -0600

    Display some data from file

--- Program.cs
+++ Program.cs
@@ -14,6 +14,52 @@ namespace MovieLibrary_HutsonRyan
 
             logger.Info("Program started");
 
+            string file = "ml-latest-small/movies.csv";
+
+            Console.WriteLine("Enter 1 to view movie library.");
+            Console.WriteLine("Enter 2 to add movie record.");
+            Console.WriteLine("Enter anything else to quit.");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+                Console.Clear();
+                if(File.Exists(file))
+                {
+                    StreamReader sr = new StreamReader(file);
+                    while (!sr.EndOfStream)
+                    {
+
+                        //read movie file
+
+                        string line = sr.ReadLine();
+                        string[] arr = line.Split(",");
+
+                        // display library
+
+                        foreach (string s in arr)
+                        {
+                            Console.WriteLine(s);
+                        }
+
+
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("File does not exist");
+                }
+
+            }
+
+            else if (input == "2")
+            {
+
+
+
+            }
 
              logger.Info("Program ended");
         }
